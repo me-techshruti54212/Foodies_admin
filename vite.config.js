bcb3f204a941d25ab5218daa8feb88182e355173
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-06-20 14:17:05 +0530

    changes done

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Foodies_admin',
-  resolve:{
-    alias:{
-      '@':path.resolve(__dirname,'./src'),
-    },
-  },
+  base:'/Foodies_admin/',
+ 
 })
